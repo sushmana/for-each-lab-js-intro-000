d96882c88c0e3b920add5ffe48f5398e9a14770a
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-16 17:34:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(element,index,array){
+  array.forEach(callback{`${index}:${element}`})
+}
+
+function iterate(callback){
+  var arr=["Hii","Hello"];
+  arr.forEach(callback);
+  return arr;
+}
+
+function doToArray(array,callback){
+  array.forEach(callback);
+}
